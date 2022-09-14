01956a7dc0ade9950faf0dfc204398b5df396888
PriscilaDamaceno <pri.damasceno@live.com>
2022-09-14 15:11:27 -0300

    else in python

---
+++ ifencade.py
@@ -0,0 +1,8 @@
+idade = int(input("Digite sua idade: "))
+nome = input("Digite seu nome: ")
+
+if idade >=18:
+    print("{} é maior de idade".format(nome))
+
+else: print("{} é menor de idade, consultar responsáveis".format(nome))
+
