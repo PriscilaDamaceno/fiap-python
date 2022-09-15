98999119d843fd4dcbfdf2daf819d0389b8b798c
PriscilaDamaceno <pri.damasceno@live.com>
2022-09-15 17:57:27 -0300

    if encadeado

---
+++ if_encadeado_2.py
@@ -0,0 +1,12 @@
+pontuacao = int(input("Informe a pontuação: "))
+
+if pontuacao > 1000:
+    print("Você ganhou 3GB de bônus!")
+else:
+    if pontuacao >500:
+        print("Você ganhou 1,5GB de bônus")
+    else:
+        if pontuacao > 200:
+            print("Você ganhou 500MB de bônus")
+        else:
+            print("Você ainda não ganhou, mas continue juntando e volte novamente!")
