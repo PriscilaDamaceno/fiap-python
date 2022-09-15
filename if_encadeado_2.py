d25823624097ec8f4316e33d8323c93ae4a55cb2
PriscilaDamaceno <pri.damasceno@live.com>
2022-09-15 18:02:21 -0300

    elif in python

--- if_encadeado_2.py
+++ if_encadeado_2.py
@@ -2,11 +2,9 @@ pontuacao = int(input("Informe a pontuação: "))
 
 if pontuacao > 1000:
     print("Você ganhou 3GB de bônus!")
+elif pontuacao >500:
+    print("Você ganhou 1,5GB de bônus")
+elif pontuacao > 200:
+    print("Você ganhou 500MB de bônus")
 else:
-    if pontuacao >500:
-        print("Você ganhou 1,5GB de bônus")
-    else:
-        if pontuacao > 200:
-            print("Você ganhou 500MB de bônus")
-        else:
-            print("Você ainda não ganhou, mas continue juntando e volte novamente!")
+    print("Você ainda não ganhou, mas continue juntando e volte novamente!")
